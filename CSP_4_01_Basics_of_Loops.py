861b674cc78518bde93c00490543a1d788da971b
emarshank <emarshank28@bentleyschool.org>
2026-01-16 12:35:52 -0800

    Add files via upload

--- CSP_4_01_Basics_of_Loops.py
+++ CSP_4_01_Basics_of_Loops.py
@@ -1,51 +1,121 @@
 #All questions must use a loop for full points.
-
+import random
 def oddNumbers(n:int) ->str:
-    """
+    answ = ""
+    for i in range (1, n, 2):
+        answ = answ + " " + str(i)
+    return answ
+
+def backwards(n)-> int:
+    count = n
+    ret = ""
+    for i in range (n):
+        ret = ret + " " + str(count)
+        count -= 1
+    return(ret)
+
+def randomRepeating():
+    ran = 0
+    count = 0
+    while ran != 10:
+        count += 1
+        ran = random.randint(1, 10)
+    #print(f"it took {count} tries to get a 10")
+    return(count)
+
+def randomRange(n):
+    hold = []
+    for i in range(0, n):
+        hold.append(random.randint(0, 100))
+    maxi = max(hold)
+    mini = min(hold)
+    return(maxi, mini)
+
+def reverse(word:str)->str:
+    hold = ""
+    count = len(word)
+    while count != 0:
+        hold = hold + word[count-1]
+        count -= 1
+    return(hold)
+
+def fizzBuzzContinuous(n):
+    an = ""
+    hold = ""
+    for i in range (1, n+1):
+        if n%3 == 0:
+            hold += "fizz"
+        if n%5 == 0:
+            hold += "buzz"
+        if n%5 != 0 and n%3 != 0:
+            hold = n
+        an += hold
+    return(an)
+
+def collatz(n):
+    an = ""
+    count = n
+    while count != 0:
+        if count % 2 == 0:
+            an = an + " " + str(int((n/2)))
+        else:
+            an = an + " " + str(((count*3)+1))
+        count -= 1
+    return(an)
+
+def fibonacci(n):
+    one = 1
+    two = 1
+    three = 1
+    hold = 0
+    an = ""
+    for i in range (n):
+        three = one + two
+        hold = two
+        two = three
+        one = hold
+        an = an + " " + str(three)
+    return(an)
+
+oddNumbers(7)
+backwards(5)
+randomRepeating()
+randomRange(5)
+reverse("Hello ")
+fizzBuzzContinuous(5)
+collatz(5)
+fibonacci(5)
+
+
+
+
+"""
     Print out all odd numbers from 1 to n(inclusive) in a single string seperated by spaces.
     example oddNumbers(5) -> "1 3 5"
     example oddNumbers(8) -> "1 3 5 7"
     example oddNumbers(-8) -> ""
-    """
-
-
-def backwards(n)-> int:
-    """
-    modify the below function such that it prints out all the numbers from n to 1
-    inclusive starting at n and counting down to 1
-    example backwards(5) -> "5 4 3 2 1"
-    example backwards(8) -> "8 7 6 5 4 3 2 1"
-    example backwards(-2) -> ""
-    """
-
-
-
-def randomRepeating():
-    """
+--
+   modify the below function such that it prints out all the numbers from n to 1
+   inclusive starting at n and counting down to 1
+   example backwards(5) -> "5 4 3 2 1"
+   example backwards(8) -> "8 7 6 5 4 3 2 1"
+   example backwards(-2) -> ""
+-- 
     Print out a random number from 1-10 until you get a 10. Then print out how many
     times it took to roll a 10
     NOTE: Given randomness no test for this question
     :return:
-    """
-    tries = 0
-    print(f"it took {tries} tries to get a 10")
-def randomRange(n):
-    """
+--
     Generate a random number from 1 to 100 n number of times. Then after that is
     done print out what the highest number and the lowers number was from the rolled numbers.
     NOTE: Given randomness no test for this question
     :param n:
     :return:
-    """
-def reverse(word:str)->str:
-    """
+--
     Takes in a string as an argument and return the given string in reverse.
     example reverse("cat") -> "tac"
     example reverse("Hello") -> "olleH"
-    """
-
-def fizzBuzzContinuous(n):
-    """
+--
     Modify the function such that it does the fizzbuzz operation on all numbers
     from 1 to n(inclusive).
     Fizz buzz is defined as
@@ -57,10 +127,7 @@ def fizzBuzzContinuous(n):
     As with above questions add each anseer to a string and return the final string.
     :param n:
     :return:
-    """
-
-def collatz(n):
-    """
+--
     Modify this function such that it mimics the collatz conjecture starting at n
     and prints out each number.
     The collatz conjecture is that if n is an even number divide it by 2. if n is
@@ -68,11 +135,7 @@ def collatz(n):
     Repeat this process until n == 1.
     :param n:
     :return:
-    """
-
-
-def fibonacci(n):
-    """
+--
     for the given argument n print out the first n numbers of the fibonacci
     sequence in a single string sperated by spaces.
     The fibonacci sequence is defined as a sequence that starts with 0 then 1 as
@@ -82,7 +145,6 @@ def fibonacci(n):
     Example fibonacci(1) -> "0"
     :param n:
     :return:
-    """
-
+   """
 
 print(fibonacci(300))
